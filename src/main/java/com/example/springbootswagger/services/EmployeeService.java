0dc5269c21be06507b0e6c4d555af8793294f3b2
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-03-13 19:51:29 +0530

    Implement service class Closes #7

---
+++ src/main/java/com/example/springbootswagger/services/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.springbootswagger.services;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 13/03/22
+ */
+public interface EmployeeService {
+}
