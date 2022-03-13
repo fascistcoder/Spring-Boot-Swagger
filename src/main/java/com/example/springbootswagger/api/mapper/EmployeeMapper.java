52f38668f3f3cb9a5e02c324d94bb2ce045f4fc8
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-03-13 19:40:17 +0530

    Implement mapper class Closes #8

---
+++ src/main/java/com/example/springbootswagger/api/mapper/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.example.springbootswagger.api.mapper;
+
+import com.example.springbootswagger.api.model.EmployeeDTO;
+import com.example.springbootswagger.api.model.EmployeeListDTO;
+import com.example.springbootswagger.domain.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 13/03/22
+ *
+ */
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
+
+    EmployeeDTO employeeToEmployeeDTO(Employee employee);
+
+    EmployeeListDTO employeeDTOToEmployee(EmployeeDTO employeeDTO);
+}
