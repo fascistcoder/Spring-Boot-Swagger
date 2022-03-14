adba4ba33ba3b6d74d6a4dcd2a2199af828d98e2
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-03-14 14:53:05 +0530

    Add controller testcase class and service class Closes #4

---
+++ src/test/java/com/example/springbootswagger/services/EmployeeServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.example.springbootswagger.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 14/03/22
+ */
+class EmployeeServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+}
